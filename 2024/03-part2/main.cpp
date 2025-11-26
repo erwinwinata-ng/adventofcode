c5941400a793d7703de4184939d3b36ec035f8e5
erwinwinata <18479920+erwinwinata-ng@users.noreply.github.com>
2025-11-27 00:14:38 +0700

    feat(2024): day 3 part 2

---
+++ 2024/03-part2/main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <regex>
+#include <sstream>
+
+int mul(int x, int y) {
+  return x * y;
+}
+
+int main() {
+  std::string input = "";
+
+  std::regex pattern("mul\\((\\d+),(\\d+)\\)|do\\(\\)|don\\'t\\(\\)");
+  auto matched_begin = std::sregex_iterator(input.begin(), input.end(), pattern);
+  auto matched_end = std::sregex_iterator();
+  int count = 0;
+  int result = 0;
+  bool is_enabled = true;
+
+  for (std::sregex_iterator i = matched_begin; i != matched_end; i++) {
+    std::smatch matched = *i;
+    std::string text = matched.str();
+
+    std::cout << text << " <- ";
+    if (text == "do()") {
+      is_enabled = true;
+    } else if (text == "don't()") {
+      is_enabled = false;
+    } else if (is_enabled) {
+      std::smatch numbers;
+      if (std::regex_search(text, numbers, pattern)) {
+        int x = std::stoi(numbers[1].str());
+        int y = std::stoi(numbers[2].str());
+
+        result += mul(x, y);
+      } else {
+        std::cout << "Pattern not found" << std::endl;
+      }
+
+      std::cout << "Matched " << ++count << " -> "<< matched.str() << std::endl;
+    }
+  } 
+
+  if (count == 0) {
+    std::cout << "No Matched" << std::endl;
+  } else {
+    std::cout << "Total: " << result << std::endl;
+  }
+
+  return 0;
+}
